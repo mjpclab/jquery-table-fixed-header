fc34ef63be6fd5264aa6690a4b78367282e7e402
marjune <marjune@163.com>
2016-10-02 20:44:47 +0800

    add support for npm and bower

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+	var sourceDirectory = 'src/';
+	var buildDirectory = 'build/';
+
+	var npmTasks = [
+		'grunt-contrib-jshint',
+		'grunt-contrib-clean',
+		'grunt-contrib-copy',
+		'grunt-contrib-uglify'
+	];
+	npmTasks.forEach(function (task) {
+		grunt.loadNpmTasks(task);
+	});
+
+	grunt.registerTask('default', ['jshint', 'clean', 'copy', 'uglify']);
+
+	grunt.config.init({
+		jshint: {
+			default: [
+				'Gruntfile.js',
+				sourceDirectory + '*.js'
+			]
+		},
+		clean: {
+			default: [buildDirectory]
+		},
+		copy: {
+			default: {
+				sourceMap: true,
+				files: [
+					{
+						expand: true,
+						cwd: sourceDirectory,
+						src: '*.js',
+						dest: buildDirectory
+					}
+				]
+
+			}
+		},
+		uglify: {
+			default: {
+				files: [
+					{
+						src: sourceDirectory + 'jquery.table-fixed-header.js',
+						dest: buildDirectory + 'jquery.table-fixed-header.min.js'
+					},
+					{
+						src: sourceDirectory + 'jquery.container-table-fixed-header.js',
+						dest: buildDirectory + 'jquery.container-table-fixed-header.min.js'
+					}
+				]
+			}
+		}
+	});
+
+};
