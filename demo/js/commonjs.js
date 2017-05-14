869e4f6015b52fc2835f8d3dbaa845849e9d16ce
marjune <marjune@163.com>
2017-05-14 21:49:42 +0800

    update commonjs example

--- demo/js/commonjs.js
+++ demo/js/commonjs.js
@@ -1,13 +1,11 @@
-var jsdom = require("jsdom");
+var JSDOM = require('jsdom').JSDOM;
 
-jsdom.env(
-	'<p>hello world</p>',
-	function (err, window) {
-		var jquery=require('jquery')(window);
-		require('../../src/jquery.table-fixed-header')(jquery);
-		console.log(jquery.fn.tableFixedHeader);
+var dom = new JSDOM('<p>hello world</p>');
+var window = dom.window;
+var jquery=require('jquery')(window);
 
-		require('../../src/jquery.container-table-fixed-header')(jquery);
-		console.log(jquery.fn.containerTableFixedHeader);
-	}
-);
+require('../../src/jquery.table-fixed-header')(jquery);
+console.log(jquery.fn.tableFixedHeader);
+
+require('../../src/jquery.container-table-fixed-header')(jquery);
+console.log(jquery.fn.containerTableFixedHeader);
